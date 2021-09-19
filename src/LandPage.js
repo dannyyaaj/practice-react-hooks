06ea06b4ef61a4593ad25ae3eb72f10ff00be0ba
Danny Yang <dannyyaaj@gmail.com>
2021-09-19 14:42:34 -0500

    add login button

--- src/LandPage.js
+++ src/LandPage.js
@@ -1,7 +1,13 @@
 import React from "react";
 
 const LandingPage = () => {
-  return <h1>Hello! You are not logged in.</h1>;
+  return  (
+    <>
+    <h1>Hello! You are not logged in.</h1>
+    <button>Log In</button>
+    </>
+  );
+
 };
 
 export default LandingPage;
