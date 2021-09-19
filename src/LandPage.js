1e30232eb3d01f232a9f21386d0a0acf0a9a1a3f
Danny Yang <dannyyaaj@gmail.com>
2021-09-19 15:04:57 -0500

    refactor to use useCallback hook

--- src/LandPage.js
+++ src/LandPage.js
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useCallback } from "react";
 
 const LandingPage = ({ setLoggedIn}) => {
+  const onLogin = useCallback(() => setLoggedIn(true), [setLoggedIn]);
   return  (
     <>
     <h1>Hello! You are not logged in.</h1>
-    <button onClick={() => setLoggedIn(true)}>Log In</button>
+    <button onClick={onLogin}>Log In</button>
     </>
   );
 
